d1bfa435353a831fc46982aead88eb8d811451b9
Kakusaretahi <144427825+Kakusaretahi@users.noreply.github.com>
2026-03-30 22:00:30 +0900

    Add files via upload

---
+++ model/factory.py
@@ -0,0 +1,26 @@
+"""
+模型工厂代码
+"""
+from abc import ABC, abstractmethod
+from typing import Optional
+from langchain_core.embeddings import Embeddings
+from langchain_community.chat_models.tongyi import BaseChatModel
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_community.embeddings import DashScopeEmbeddings
+from utils.config_handler import rag_conf
+
+class BaseModelFactory(ABC):
+    @abstractmethod
+    def generator(self) -> Optional[Embeddings | BaseChatModel]:
+        pass
+
+class ChatModelFactory(BaseModelFactory):   #聊天模型工厂
+    def generator(self) -> Optional[Embeddings | BaseChatModel]:
+        return ChatTongyi(model = rag_conf["chat_model_name"])
+
+class EmbeddingsFactory(BaseModelFactory):  #嵌入模型工厂
+    def generator(self) -> Optional[Embeddings | BaseChatModel]:
+        return DashScopeEmbeddings(model = rag_conf["embedding_model_name"])
+
+chat_model = ChatModelFactory().generator()
+embed_model = EmbeddingsFactory().generator()
